a3b5864e786ebd7cb411592a2d7c35b20b6bbfd6
edebrouwert <115546792+edebrouwert@users.noreply.github.com>
2022-11-02 10:22:37 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,15 +1,17 @@
 __winc_id__ = "ae539110d03e49ea8738fd413ac44ba8"
 __human_name__ = "files"
 
-#Exercise 1
 import os
 import shutil
+
+cwd = os.getcwd()
+path_to_cache = os.path.join(cwd, r'cache')
+
+#Exercise 1
 def clean_cache():
-    current_directory = os.getcwd()
-    new_directory = os.path.join(current_directory, r'cache')
-    if os.path.exists(new_directory):
-        shutil.rmtree(new_directory)
-    os.mkdir(new_directory)
+    if os.path.exists(path_to_cache):
+        shutil.rmtree(path_to_cache)
+    os.mkdir(path_to_cache)
 
 #Exercise 2
 def cache_zip(zipfilepath, cachedirpath):
@@ -17,7 +19,7 @@ def cache_zip(zipfilepath, cachedirpath):
 
 #Exercise 3
 def cached_files():
-    filelist = [os.path.abspath(i) for i in os.scandir(r"C:\Users\erwin\Documents\Winc\files\cache")]
+    filelist = [os.path.abspath(i) for i in os.scandir(path_to_cache)]
     return filelist
 
 #Exercise 4
@@ -28,6 +30,5 @@ def find_password(filepathlist):
         filecontent = file.readlines()  #put every line in the file into a list of strings
         for i in filecontent:           #loop over list and check if the word password is in item. If so, set password
             if "password" in i:        
-                password = i
+                password = i.split()[1]
     return password
-
